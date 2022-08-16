5e259041dc4f3a0a9cf475ba475455bb689e1660
willdrr <willxdd97@gmail.com>
2022-08-16 03:22:51 +0000

    Configuring user views

--- Gemfile
+++ Gemfile
@@ -82,3 +82,5 @@ gem 'capistrano-rails', '~> 1.4'
 gem 'capistrano-passenger', '~> 0.2.0'
 
 gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
+
+gem "devise", "~> 4.8"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
 
-  # Defines the root path route ("/")
+  devise_for :users
+
   # root "articles#index"
 end
