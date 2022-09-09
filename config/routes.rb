d53a9df1ead1ec4eee6138ab652e7f5d7467bcac
willdrr <willxdd97@gmail.com>
2022-09-09 22:12:38 +0000

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
+
   devise_scope :user do
     get "users", to: "devise/sessions#new"
   end
 
   devise_for :users
 
-  root "home#index"
+  root "home#index", to: '/'
 end
